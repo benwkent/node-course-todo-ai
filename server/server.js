ccba5dadee5ae25060abe9f8e1a7da0e925e1790
Kent <BKent@dentrix.com>
2018-12-31 15:31:15 -0700

    Add GET /users/me and auth middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
+var {authenticate} = require('./middleware/authenticate');
+
 var app = express();
 const port = process.env.PORT;
 
@@ -105,6 +107,10 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
