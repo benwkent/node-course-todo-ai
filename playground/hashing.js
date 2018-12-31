6750d3dd9d871350102825e100b2922669913399
Kent <BKent@dentrix.com>
2018-12-31 15:47:05 -0700

    Salt/hash user PW

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,4 +1,17 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-jws.sign
+var password = '12345asdf';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$Ck3kC7XbmwN3vInv/mujL.GZWn.PSx0f2WYUzj7YKRyLdk1n3lL.S';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
